3e150fe1dbca1185925074acbed845e994fe10da
shijuvar <shiju.varghese@outlook.com>
2014-08-27 09:22:38 +0530

    code refactor

--- JavaScript/primitivetypes.js
+++ JavaScript/primitivetypes.js
@@ -1,11 +1,14 @@
-console.log( typeof "ShijuVar");
-console.log( typeof 100);
-console.log( typeof 10.5);
-console.log( typeof true);
+var user="shijuvar";
+var num=100;
+var num1=10.5;
+var enable=true;
+console.log( typeof user);
+console.log( typeof num);
+console.log( typeof num1);
+console.log( typeof enable);
 console.log( typeof undefined);
 console.log( typeof null);
 
 //The following code block will be use Primitive wrapper types at run time
-var name="shijuvar";
-var firstChar=name.charAt(0);
+var firstChar=user.charAt(0);
 console.log(firstChar);
